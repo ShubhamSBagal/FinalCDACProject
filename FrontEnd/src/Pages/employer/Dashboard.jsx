5026b86c5b6bd6491a5f1f1de8815506268d6045
ShubhamSBagal <shubhambagal333@gmail.com>
2023-08-29 17:38:15 +0530

    first commit

---
+++ FrontEnd/src/Pages/employer/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Dashboard() {
+  return (
+    <>
+      <center><h1 >Welcome to the Dashboard</h1></center><br/><br/><br/>
+      <center> <div className='col-md-8'>
+      <img height={700}  src="/Assets/Images/HomePage/com.png"></img>
+
+      </div></center>
+    </>
+  );
+}
